53d0a9a9d99f075f0ea076ba275fc2a148c643b0
tahinaBemax <tahina.nambinintsoa@passerellesnumeriques.org>
2025-09-01 09:17:24 +0300

    mise à jour mcd

--- backend/gestion-livraison-de-colis-api/src/common/dto/point-livraison/point-livraison-create-dto.ts
+++ backend/gestion-livraison-de-colis-api/src/common/dto/point-livraison/point-livraison-create-dto.ts
@@ -1,43 +1,56 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsNumberString, IsOptional } from "class-validator";
 
 export class CreatePointLivraisonDto {
     @IsOptional()
     @IsNumber()
+    @ApiProperty({required: false})
     id?: number;
-
+    
     @IsNotEmpty()
+    @ApiProperty()
     numero_magasin: string;
-
+    
     @IsNotEmpty()
+    @ApiProperty()
     nom_rue: string;
-
+    
     @IsNotEmpty()
+    @ApiProperty()
     numero_rue: string;
-
+    
     @IsNotEmpty()
     @IsOptional()
+    @ApiProperty({required: false})
     departement?: string;
     
     @IsNotEmpty()
+    @ApiProperty()
     ville: string;
     
     @IsNotEmpty()
     @IsOptional()
+    @ApiProperty({required: false})
     pays?: string;
     
     @IsNumber()
     @IsOptional()
+    @ApiProperty({required: false})
     latitude?: number;
     
     @IsNumber()
     @IsOptional()
+    @ApiProperty({required: false})
+    @ApiProperty({required: false})
     longitude?: number;
     
     @IsNotEmpty()
     @IsNumberString()
+    @ApiProperty()
     code_postal: string;
     
     @IsOptional()
     @IsOptional()
+    @ApiProperty({required: false})
     complement_adresse?: string;
 }
