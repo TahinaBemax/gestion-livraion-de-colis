9218a6c39ef148a2593e15c345b7a4b758c371ba
tahinaBemax <tahina.nambinintsoa@passerellesnumeriques.org>
2025-09-19 09:48:51 +0300

    bug fix port

--- backend/gestion-livraison-de-colis-api/src/main.ts
+++ backend/gestion-livraison-de-colis-api/src/main.ts
@@ -51,7 +51,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.CLIENT_PORT ?? 3000);
+  await app.listen(process.env.SERVER_PORT ?? 3000);
 }
 
 bootstrap();
